7b43598352012e06a6057ccdfd86a9d00e5da79e
Кристина Волосова <justanobody017527@gmail.com>
2024-11-30 12:40:06 +0300

    Classwork DONE

--- Processes/Program.cs
+++ Processes/Program.cs
@@ -16,7 +16,7 @@ namespace Processes
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			Application.Run(new MainForm());
 		}
 	}
 }
